14f87b3253b8f6a0fbe7fe9c1dc1de442c43e171
KEVYN DOS SANTOS CAMILO <kevyn.scamilo@senacsp.edu.br>
2024-08-20 10:45:22 -0300

    Test

--- SysPecNSLib/Banco.cs
+++ SysPecNSLib/Banco.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SysPecNSLib
 {
-    public class Banco
+    /// <summary>
+    /// Classe estatica (Não neccesita de instancia) de conexão de banco de dados
+    /// </summary>
+    public static class Banco // Classe de conexão de banco de dados
     {
+        /// <summary>
+        /// Método Abrir da classe Banco é utilizado para representar comandos SQL no
+        /// Servidor informado, cujos dados foram informados na string de conexão(strconn)
+        /// </summary>
+        /// <returns>Entrega um objeto de comandos SQL, que serão executados na conexão</returns>
+        public static MySqlCommand Abrir() // Metodo Conexão
+        {
+            // dados da conexão
+            string strconn = @"server=127.0.0.1;database=syspecdb;user=root;password=";
+            MySqlConnection cn = new MySqlConnection(strconn);
+            MySqlCommand cmd = new();
+            try //metodo de tramento de excessção
+            {
+                cn.Open();
+                cmd.Connection = cn;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return cmd;
+        }
+
+
     }
 }
--- SysPecNSLib/Nivel.cs
+++ SysPecNSLib/Nivel.cs
@@ -8,5 +8,65 @@ namespace SysPecNSLib
 {
     public class Nivel
     {
+        // --Atributos (campo)--
+        
+
+
+        // --Propriedades--
+        // --Metodos de acesso (set e get)--
+               
+        public int Id {  get; set; }
+        public string? Nome { get; set; }
+        public string? Sigla { get; set; }
+
+
+
+        // --Metodos Construtores--
+        public Nivel() { }
+
+        public Nivel(string nome, string sigla) // --SobreCarga de Métodos
+        { 
+            Nome = nome;
+        }
+
+        public Nivel(int id, string? nome, string? sigla)
+        {
+            Id = id;
+            Nome = nome;
+            Sigla = sigla;
+        }
+
+        // --Métodos (Requisitos)--
+        /// <summary>
+        /// Método de inserir registro de nivel no banco de dados
+        /// </summary>
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+
+        }
+
+        public static Nivel ObterPorId(int id)
+        {
+            Nivel nivel = new();
+            // Consulta no banco e retorna o Nivel
+            return nivel;
+        }
+
+        public static List<Nivel> ObterLista()
+        {
+            List<Nivel> lista = new();
+            // consultar para retornar a lista de niveis
+            return lista;
+        }
+
+        public bool Atualizar()
+        {
+            return true;
+        }
+        public void Excluir(int id)
+        {
+
+        }
     }
 }
