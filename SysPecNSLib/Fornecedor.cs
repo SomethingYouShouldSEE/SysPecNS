601b23454f65d28fdef60e6a3379428f0e0156a1
KEVYN DOS SANTOS CAMILO <kevyn.scamilo@senacsp.edu.br>
2024-09-04 11:55:36 -0300

    ASD

--- SysPecNSLib/Fornecedor.cs
+++ SysPecNSLib/Fornecedor.cs
@@ -8,5 +8,40 @@ namespace SysPecNSLib
 {
     public class Fornecedor
     {
-    }
+        public int Id { get; set; }
+        public string RazaoSocial{ get; set; }
+        public string Fantasia { get; set; }
+        public char CNPJ { get; set; }
+        public string? Contato { get; set; }
+        public string? Telefone { get; set; }
+        public string? Email { get; set; }
+
+        public Fornecedor()
+        {
+
+        }
+
+        public Fornecedor(int id, string razaosocial, string fantasia, char cnpj, string contato, string telefone, string email)
+        {
+            Id = id;
+            RazaoSocial = razaosocial;
+            Fantasia = fantasia;
+            CNPJ = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+
+        public Fornecedor(string razaosocial, string fantasia, char cnpj, string contato, string telefone, string email)
+        {
+            RazaoSocial = razaosocial;
+            Fantasia = fantasia;
+            CNPJ = cnpj;
+            Contato = contato;
+            Telefone = telefone;
+            Email = email;
+        }
+
+        
+    }   
 }
